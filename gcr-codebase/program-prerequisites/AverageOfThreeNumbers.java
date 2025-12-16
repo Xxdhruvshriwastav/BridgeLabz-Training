42f68aab4d8c2bc5a66b9c2c3e5c791aa7d1f25c
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-16 16:56:43 +0530

    added 8th

---
+++ gcr-codebase/program-prerequisites/AverageOfThreeNumbers.java
@@ -0,0 +1,10 @@
+class AverageOfThreeNumbers {
+  public static void main(String[] args) {
+    double num1  = 10.5;
+    double num2  = 20.5;
+    double num3  = 30.0;
+
+    double avg = num1 + num2 + num3 /3;
+    System.out.println("The average of three numbers is: " + avg);
+  }
+}
