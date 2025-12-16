cdcec2864e6f95b5224f4e737e28a90605c999e6
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-16 16:59:27 +0530

    added 9 th

---
+++ gcr-codebase/program-prerequisites/ConvertKIlometerToMiles.java
@@ -0,0 +1,9 @@
+class ConvertKIlometerToMiles {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter distance in kilometers: ");
+    double kilometers = sc.nextDouble();
+    double miles = kilometers * 0.621371;
+    System.out.println(miles);
+  }
+}
