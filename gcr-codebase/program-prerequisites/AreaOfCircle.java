916e560c192288939c1204d34f866c74ce94bd40
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-16 16:42:21 +0530

    [ashish] added 4th  program sucessfully

---
+++ gcr-codebase/program-prerequisites/AreaOfCircle.java
@@ -0,0 +1,7 @@
+class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle with radius " + radius + " is: " + area);
+    }
+}
