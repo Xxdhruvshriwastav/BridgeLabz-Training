baa2051ac66cbf1e76ea267c7fb6a7d5b52e719a
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-16 16:52:58 +0530

    7th program

---
+++ gcr-codebase/program-prerequisites/PerimeterOfRectange.java
@@ -0,0 +1,10 @@
+class PerimeterOfRectange { 
+    public static void main(String[] args) {
+        double length = 30.5;
+        double breadth = 20.5;
+
+        double perimeter = 2 * (length + breth);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+
+}
