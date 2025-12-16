9d621ebff4d50a4d590ac5176ffb8fbad5414f9d
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-16 17:01:44 +0530

    added 10th

---
+++ gcr-codebase/program-prerequisites/PowerCalculation.java
@@ -0,0 +1,15 @@
+class PowerCalculation {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the base number: ");
+    int base = sc.nextInt();
+    System.out.print("Enter the exponent: ");
+    int exponent = sc.nextInt();
+    int result = 1;
+    for (int i = 1; i <= exponent; i++) {
+      result *= base;
+    }
+    System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+  }
+
+}
