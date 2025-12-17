c0f866158e74406f4df7eb773dc4f84507ac277a
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 16:05:38 +0530

    [ashish] : solved HeightInCentimeter problem

---
+++ gcr-codebase/programming-element/Level1/HeightInCentimeter.java
@@ -0,0 +1,19 @@
+        public class HeightInCentimeter {
+
+	public static void main (String [] args ) {
+
+		// Assigning the value of the variable  
+		int height = 5;
+		int inches = 10;
+
+ 		// calculating the height in inches note it 1 foot = 12 inches and 1 inch = 2.54 cm
+
+		double heightINCentimeter = (feet * 12 + inches) * 2.54;
+                
+                // printing the values 	
+
+		System.out.println("Height in cm = " + heightINCentimeter); 
+
+	}
+
+}
