5365942a0822e56bdd5c2177d9f8f80f2b4ebbb5
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:48:35 +0530

    Fix variable naming for profit percentage calculation

--- gcr-codebase/programming-element/Level1/ProfitPercentage.java
+++ gcr-codebase/programming-element/Level1/ProfitPercentage.java
@@ -2,13 +2,15 @@ public class ProfitPercentage {
 
 public static void main (String [] args ) {
 
+// Assiging the value of variables
 double cost_price = 129;
 double selling_price = 191;
 double profit = selling_price -cost_price;
 
-double profit_percentage = profit/cost_price * 100;
+// Calculating the profit percentage  
+double profitPercentage = profit/cost_price * 100;
 
-System.out.println(profit_percentage + "%");
+System.out.println(profitPercentage + "%");
 
 }
 
