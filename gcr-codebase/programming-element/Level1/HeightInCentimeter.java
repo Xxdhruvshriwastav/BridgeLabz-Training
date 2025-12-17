aad21f2390798a53a89c370c52793bfaca52c4f7
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 21:41:25 +0530

    Rename variable 'height' to 'feet'

--- gcr-codebase/programming-element/Level1/HeightInCentimeter.java
+++ gcr-codebase/programming-element/Level1/HeightInCentimeter.java
@@ -3,7 +3,7 @@
 	public static void main (String [] args ) {
 
 		// Assigning the value of the variable  
-		int height = 5;
+		int feet = 5;
 		int inches = 10;
 
  		// calculating the height in inches note it 1 foot = 12 inches and 1 inch = 2.54 cm
