a190dc8afc3974587bcebec46f3382e828720b29
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 12:56:20 +0530

    [ashish] : adding level 2 folder

---
+++ gcr-codebase/programming-element/Level2/CalculateSimpleIntrest.java
@@ -0,0 +1,9 @@
+class CalculateSimpleIntrest{
+  public static void main(String[] args) {
+    double principal = 10010;
+    double rate = 5.5;
+    double time = 20;
+
+    double simpleIntrest = (principal * rate * time) / 100;
+  }
+}
