d7eb2685ddf939e7f627abbae11ac16831c3e38a
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:34:41 +0530

    Adding the comments

--- gcr-codebase/programming-element/Level1/Discount.java
+++ gcr-codebase/programming-element/Level1/Discount.java
@@ -1,18 +1,17 @@
 public class Discount {
 
 public static void main (String [] args ) {
+       // Assiging the value of variables
+       int fee = 125000;
+       int discountPercent = 10;
 
-int fee = 125000;
-int discountPercent = 10;
-
-int discount = fee/100 * 10;
+       // calculating the discounts
+       int discount = fee/100 * 10;
  
 
-
-System.out.println(discount);
-
-
-}
+       // print the value of discount
+       System.out.println(discount);
 
 
+      }
 }
