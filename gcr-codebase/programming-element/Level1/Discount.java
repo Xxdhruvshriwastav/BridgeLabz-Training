0544260fac6cefe323d30c011332b7df441377dd
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 15:12:23 +0530

    [ashish] : solved discount problem

---
+++ gcr-codebase/programming-element/Level1/Discount.java
@@ -0,0 +1,18 @@
+public class Discount {
+
+public static void main (String [] args ) {
+
+int fee = 125000;
+int discountPercent = 10;
+
+int discount = fee/100 * 10;
+ 
+
+
+System.out.println(discount);
+
+
+}
+
+
+}
