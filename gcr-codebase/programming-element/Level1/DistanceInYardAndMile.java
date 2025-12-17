f5ca19ca5f8b6b8f0f36fa60bce4e2bb6f02cb0d
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 16:46:32 +0530

    [ashish] : solved DistanceInYardAndMile problem

---
+++ gcr-codebase/programming-element/Level1/DistanceInYardAndMile.java
@@ -0,0 +1,43 @@
+        import java.util.Scanner;
+
+	public class DistanceInYardAndMile{
+
+	public static void main (String [] args ) {
+
+		Scanner sc = new Scanner(System.in);
+            
+                // taking user input of distance in feet
+
+                System.out.println("put the length in feet");
+
+		// taking Input from the user
+
+                int distance = sc.nextInt();
+
+ 		// Calculating Yard distance 
+
+		double yardDistance = distance/3.0;
+  
+  		 
+		// Calculating the miles distance 
+                double mileDistance = distance/5280.0;
+   
+
+		
+                // printing the Yard distance
+		
+		System.out.println(yardDistance);
+		
+		// printing the miles distance
+
+                System.out.println(mileDistance);
+
+		// closing the scanner object 
+		sc.close(); 
+                
+
+                
+
+	}
+
+}
