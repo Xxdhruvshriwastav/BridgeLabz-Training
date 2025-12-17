125345a7ccd3136a09a6e92bb0e2ccfff6262a20
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 13:14:14 +0530

    [ashish] : adding find Age program

---
+++ gcr-codebase/programming-element/Level1/FindAge.java
@@ -0,0 +1,14 @@
+public class FindAge {
+
+public static void main(String [] args){
+
+int birthyear = 2000;
+int currentyear = 2024;
+
+int age =  currentyear - birthyear ;
+
+System.out.println(age);
+
+
+}
+}
