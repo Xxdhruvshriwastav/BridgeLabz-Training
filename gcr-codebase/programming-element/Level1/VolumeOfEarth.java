32cb4af39c3efb4728b21a518f09cda3803ca30e
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:50:53 +0530

    Fix formatting and improve volume calculation output

--- gcr-codebase/programming-element/Level1/VolumeOfEarth.java
+++ gcr-codebase/programming-element/Level1/VolumeOfEarth.java
@@ -2,16 +2,18 @@ public class VolumeOfEarth {
 
 public static void main (String [] args ) {
 
+// Assigning the value of variables    
 double radius =  6378; // in km 
 double pi = 3.14;  
 
+// calculating the value of volumeOfEarth, volumeInCubicMiles     
 double volumeOfEarth = (4.0/3) * pi * radius * radius * radius;
 double volumeInCubicMiles = volumeOfEarth * 0.239913;
 
+// Printing the value of volumeOfEarth, volumeInCubicMiles 
 System.out.println("Volume of Earth = " + volumeOfEarth + " cubic km"); 
 System.out.println("Volume of Earth = " + volumeInCubicMiles + " cubic miles");
     
 
-}
-
+    }
 }
