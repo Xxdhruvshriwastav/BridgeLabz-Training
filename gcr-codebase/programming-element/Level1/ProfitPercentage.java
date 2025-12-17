4410a4c65942f6ea05498aed9dea9accdc49b263
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 14:53:12 +0530

    [ashish] : solve the profit percentage question

---
+++ gcr-codebase/programming-element/Level1/ProfitPercentage.java
@@ -0,0 +1,16 @@
+public class ProfitPercentage {
+
+public static void main (String [] args ) {
+
+double cost_price = 129;
+double selling_price = 191;
+double profit = selling_price -cost_price;
+
+double profit_percentage = profit/cost_price * 100;
+
+System.out.println(profit_percentage + "%");
+
+}
+
+
+}
