377a3384f8c8f81f9627a32d0582f34417f64e68
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:54:42 +0530

    Refactor variable names for consistency

--- gcr-codebase/programming-element/Level1/Average.java
+++ gcr-codebase/programming-element/Level1/Average.java
@@ -2,17 +2,18 @@ public class Average {
 
 public static void main (String [] args ) {
 
-int phy_marks = 95;
-int mat_marks = 94;
-int che_marks = 96;
+// Assiging the variable values
+int phyMarks = 95;
+int matMarks = 94;
+int cheMarks = 96;
 
-int total_marks = phy_marks + mat_marks + che_marks;
+// Calculating the total marks  
+int totalMarks =  phyMarks + matMarks + cheMarks;
 
-int average = total_marks/3;
+int average = totalMarks/3;
 
+// printing the values  
 System.out.println(average);
 
-}
-
-
+    }
 }
