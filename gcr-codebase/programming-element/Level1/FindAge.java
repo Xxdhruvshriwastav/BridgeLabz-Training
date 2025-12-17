79df7639d64fa59a039cc3d1d6f4101a9994fcbc
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:43:45 +0530

    Fix formatting and remove extra lines in FindAge.java

--- gcr-codebase/programming-element/Level1/FindAge.java
+++ gcr-codebase/programming-element/Level1/FindAge.java
@@ -1,14 +1,13 @@
 public class FindAge {
 
 public static void main(String [] args){
-
+// Assiging the value of the variables
 int birthyear = 2000;
 int currentyear = 2024;
-
+// calculating the current age
 int age =  currentyear - birthyear ;
 
 System.out.println(age);
-
-
-}
-}
+  
+   }
+ }
