a88470f51f31bc71998df61d24d5d6c2052e049e
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:31:27 +0530

    Add comments to CalculateSimpleIntrest.java
    Added comments for variable assignment and calculation.

--- gcr-codebase/programming-element/Level1/CalculateSimpleIntrest.java
+++ gcr-codebase/programming-element/Level1/CalculateSimpleIntrest.java
@@ -1,9 +1,12 @@
 class CalculateSimpleIntrest{
   public static void main(String[] args) {
+    //assigning the variable values
     double principal = 10010;
     double rate = 5.5;
     double time = 20;
-
+    //  Calculating the simple intrest 
     double simpleIntrest = (principal * rate * time) / 100;
+
+    System.out.println("simpleIntrest");
   }
 }
