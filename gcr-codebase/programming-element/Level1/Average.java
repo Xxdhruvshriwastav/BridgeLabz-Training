240a2182c26524d852b70aa1d9af0097eeac3f31
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 13:25:02 +0530

    [ashish] : adding find average no

---
+++ gcr-codebase/programming-element/Level1/Average.java
@@ -0,0 +1,18 @@
+public class Average {
+
+public static void main (String [] args ) {
+
+int phy_marks = 95;
+int mat_marks = 94;
+int che_marks = 96;
+
+int total_marks = phy_marks + mat_marks + che_marks;
+
+int average = total_marks/3;
+
+System.out.println(average);
+
+}
+
+
+}
