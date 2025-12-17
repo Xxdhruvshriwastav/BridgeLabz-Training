3397c0d95d1bab3da6b17b08712e75e73992e211
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 16:25:29 +0530

    [ashish] : solved length of square problem

---
+++ gcr-codebase/programming-element/Level1/PerimeterOfSquare.java
@@ -0,0 +1,21 @@
+        public class PerimeterOfSquare {
+
+	public static void main (String [] args ) {
+
+		// Assigning the value of the variable  
+		int perimeterOfSquare = 100;
+
+ 		// Athematic operation 
+
+		int side = perimeterOfSquare/4;
+
+                // printing the perimeterOfSquare
+		
+		System.out.println("length of Square is  " + side); 
+                
+
+                
+
+	}
+
+}
