44254ffe4cc3a74530faf9ec096955bf02eea7fe
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-17 15:05:53 +0530

    [ashish] : ading after doing another approch

---
+++ gcr-codebase/programming-element/Level1/EqualDistributionAndRemaing2.java
@@ -0,0 +1,19 @@
+public class EqualDistributionAndRemaing2 {
+
+public static void main (String [] args ) {
+
+int pen = 14;
+int student = 3;
+
+int equal_distribution = pen/student;
+
+int remaining_pen = pen % student;
+
+System.out.println("total equal_distribution is " 
+    + equal_distribution + "remaining_pen" + remaining_pen);
+
+
+}
+
+
+}
