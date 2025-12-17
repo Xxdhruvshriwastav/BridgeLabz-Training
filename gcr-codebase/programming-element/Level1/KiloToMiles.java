b361ccb98eaf9d673209bd8c8a3276345f2d0beb
Dhruv <126322530+Xxdhruvshriwastav@users.noreply.github.com>
2025-12-17 19:45:39 +0530

    Add comments for clarity in KiloToMiles.java
    Added comments to clarify variable assignment and output.

--- gcr-codebase/programming-element/Level1/KiloToMiles.java
+++ gcr-codebase/programming-element/Level1/KiloToMiles.java
@@ -1,13 +1,12 @@
 public class KiloToMiles {
 
 public static void main (String [] args ) {
-
+ // Assigning the value of variables 
 double km = 10.8;
 double result = 1.6 * km;
 
+// printing the values of result  
 System.out.println(result);
 
-}
-
-
+    }
 }
