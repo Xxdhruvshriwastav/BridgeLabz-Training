0238c1b704afa0138c3e3a2c07d206e93013206a
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-18 12:35:25 +0530

    [ashish] : added the program of findout the natural no

---
+++ gcr-codebase/control-flow/Level1/CheckNaturalNo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CheckNaturalNo {
+
+	public static void main(String[] args) {
+
+		// creating the Scanner object
+
+		Scanner sc = new Scanner(System.in);
+
+		// Taking input from the user
+
+		System.out.println("Enter the no ");
+
+		int n = sc.nextInt();
+
+		int naturalNo = n * (n+1) / 2;
+
+           	
+		if (n < 0 ) {
+		
+                System.out.println("Enter the no ");
+
+		} else {
+			
+		System.out.println("The sum of " + n + "natural numbers is" + naturalNo);
+
+		}
+		
+		
+		// closing the Scanner object
+		sc.close();
+
+	}
+
+}
