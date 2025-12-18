1b94bf851f8fb321ed09c09a6a1f580bdde1ca99
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-18 12:22:48 +0530

    [ashish] : added the program Largest no findout

---
+++ gcr-codebase/control-flow/Level1/LargestNo.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class LargestNo {
+
+	public static void main(String[] args) {
+
+		// creating the Scanner object
+
+		Scanner sc = new Scanner(System.in);
+
+		// Taking input from the user
+
+		System.out.println("Enter the no 1");
+
+		int num1 = sc.nextInt();
+		
+		System.out.println("Enter the no 2");
+
+		int num2 = sc.nextInt();
+
+
+		System.out.println("Enter the no 3");
+
+		int num3 = sc.nextInt();
+
+		
+
+		// cheking the smallest no 
+
+		if ( num1 >= num2 && num1 >= num3 ) {
+
+			System.out.println("Largest no is number 1 ");
+
+		} else if ( num2 >= num1 && num2 >= num3 ){
+
+			System.out.println("Largest no  is number 2");
+
+		}
+		
+		else  {
+
+		        System.out.println("Largest no is number 3");
+		}
+      
+		// closing the Scanner object
+		sc.close();
+
+	}
+
+}
