e2e93bfebda72e3168c25cf95b24c1fe4d7b14c0
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-19 12:34:08 +0530

    [ashish] : added the program Divisible by 5FindMultipleUsingWhileLoop

---
+++ gcr-codebase/control-flow/Level2/FindMultipleUsingWhileLoop.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class FindMultipleUsingWhileLoop {
+
+	public static void main(String[] args) {
+
+		// creating the Scanner object
+
+		Scanner sc = new Scanner(System.in);
+
+		// Taking input from the user
+
+		System.out.println("Enter the number");
+		
+		int number = sc.nextInt();
+		
+		System.out.println("Enter the base");
+		
+		
+		
+		int i = 1;
+
+		if (number > 0 && number < 100  ) {
+
+	        while (number > i) {
+		 
+		if ( number % i ==  0 ) {
+		
+		
+		System.out.println("This no is divisible by " + i);
+
+		
+					}
+		i++;
+				          }
+					     }
+
+		
+                                
+		// closing the Scanner object
+		sc.close();
+
+	}
+
+}
