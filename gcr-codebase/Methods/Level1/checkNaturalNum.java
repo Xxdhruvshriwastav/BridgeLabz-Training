f0d59db915c1f7961e2791b136d5dc4fb5a04353
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2025-12-20 16:03:01 +0530

    [ashish] : added the program of Natural no calculation

---
+++ gcr-codebase/Methods/Level1/checkNaturalNum.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class NaturalNo {
+
+
+       // declaring the check sum of natural num..
+	 
+       public static void checkNaturalNum(int num) {
+	
+	  // condition check negative zero or negative or positive
+	  
+	  int sum = 0;
+	  
+	  for (int i = 1; i <= num; i++){
+	  
+	  sum += i;
+	  }
+	  
+	  
+	
+	}
+ 
+    // main method
+	
+    public static void main(String[] args) {
+   
+        // creating the scanner object
+		
+        Scanner sc = new Scanner(System.in);
+		
+		// taking input from the user
+		
+		System.out.println("Enter the value of number ");
+		int num = sc.nextInt();
+		
+       
+
+		
+        checkNaturalNum( num);
+		
+		// closing the scanner object
+		// sc.close();
+		
+    }
+}
