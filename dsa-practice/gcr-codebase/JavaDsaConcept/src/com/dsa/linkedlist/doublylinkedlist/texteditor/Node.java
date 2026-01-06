f251e7a1c988a7bbfc8d8d25297bbfdf8a42ebed
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-06 17:20:00 +0530

    [Ashish ]: addded program of texteditor

---
+++ dsa-practice/gcr-codebase/JavaDsaConcept/src/com/dsa/linkedlist/doublylinkedlist/texteditor/Node.java
@@ -0,0 +1,13 @@
+package com.dsa.linkedlist.doublylinkedlist.texteditor;
+
+public class Node {
+	 String content;
+	 Node prev;
+	 Node next;
+
+	 Node(String content) {
+		 this.content = content;
+		 this.prev = null;
+		 this.next = null;
+	 }
+}
