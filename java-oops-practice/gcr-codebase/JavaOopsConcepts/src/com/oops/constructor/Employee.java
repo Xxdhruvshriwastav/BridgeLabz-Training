cb34c2bdfe1cb26fd270726be2ee215d9ab130bf
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-06 22:34:21 +0530

    [Ashish]: Added the program of constructor name Employee.

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/oops/constructor/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/oops/constructor/Employee.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.oops.constructor.levelone;
+package com.oops.constructor;
 
 public class Employee {
 
@@ -25,7 +25,7 @@ public class Employee {
             this.salary = salary;
         } else {
             System.out.println("Invalid salary amount");
-        }x
+        }
     }
 	
     public static void main(String[] args) {
