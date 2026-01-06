7b4977df080d2d9dfda83a42b841edfcfc52cbfc
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-07 01:18:29 +0530

    [Ashish] : added the program of User.java

--- java-oops-practice/seneriobased/SeneriobasedQuestions/src/day4/User.java
+++ java-oops-practice/seneriobased/SeneriobasedQuestions/src/day4/User.java
@@ -1,15 +1,40 @@
 package day4;
 
 public class User {
-	 protected String name;
-	 private String email; // imp data
+	
+	protected String name;
+	private String age;
+	private String adoptedPetType;
+	private String adoptedPetName;
+	private Pet adoptedPet;
+	
+	
+	public User(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void adoptPet(Pet pet) {
+		this.adoptedPet = pet;
+		this.adoptedPetType = pet.getClass().getSimpleName();
+		this.adoptedPetName = pet.getName();
+	}
+	
+	public void showAdoptedPet() {
+		System.out.println(name + " Adopted " + adoptedPetType + " of name " + adoptedPetName);
+	}
+	
+	public void interactWithPet() {
+        adoptedPet.makeSound();
+        adoptedPet.feed();
+        adoptedPet.play();
+        adoptedPet.sleep();
+        adoptedPet.showStatus();
+    }
 
-	 public User(String name, String email) {
-		 this.name = name;
-		 this.email = email;
-	 }
-
-	 public String getName() {
-		 return name;
-	 }
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
