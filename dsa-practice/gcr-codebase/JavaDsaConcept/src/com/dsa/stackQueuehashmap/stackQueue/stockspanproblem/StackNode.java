4057a6bac133bc0930cf428482b3c9aafc694717
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-06 20:09:11 +0530

    [Ashish ]: solve the program of Stack & Queue of sliding stock span

---
+++ dsa-practice/gcr-codebase/JavaDsaConcept/src/com/dsa/stackQueuehashmap/stackQueue/stockspanproblem/StackNode.java
@@ -0,0 +1,12 @@
+package com.dsa.stackQueuehashmap.stackQueue.stockspanproblem;
+
+public class StackNode {
+	
+	public int[] data;
+	public StackNode next;
+	
+	public StackNode(int[] data) {
+		this.data = data;
+		this.next = next;
+	}
+}
