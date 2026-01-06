6fed7ebf12bc488e33ead7451c41c7b5781adf60
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-06 20:04:04 +0530

    [Ashish ]: solve the program of Stack & Queue of sliding window

---
+++ dsa-practice/gcr-codebase/JavaDsaConcept/src/com/dsa/stackQueuehashmap/stackQueue/slidingwindow/StackNode.java
@@ -0,0 +1,12 @@
+package com.dsa.stackQueuehashmap.stackQueue.slidingwindow;
+
+public class StackNode {
+	
+	public int index;
+	public StackNode next;
+	public StackNode prev;
+	
+	public StackNode(int index) {
+		this.index = index;
+	}
+}
