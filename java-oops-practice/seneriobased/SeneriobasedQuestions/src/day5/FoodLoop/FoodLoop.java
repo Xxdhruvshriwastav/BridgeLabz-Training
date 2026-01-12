59d00027e7e4de53958dded39057f9987e1d587d
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-12 11:21:09 +0530

    [Ashish] : Added the program of and modified this FoodLoop

--- java-oops-practice/seneriobased/SeneriobasedQuestions/src/day5/FoodLoop/FoodLoop.java
+++ java-oops-practice/seneriobased/SeneriobasedQuestions/src/day5/FoodLoop/FoodLoop.java
@@ -1,13 +1,25 @@
 package day5.FoodLoop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodLoop {
 
-	public static void main (String [] args) {
-		
-		FoodItem f1 = new VegItem("DalMakhni", "veg", 100, 10);
-		FoodItem f2 = new VegItem("Butter-Panner", "veg", 1000, 10);
-		
-		
-	}
-	
+    public static void main(String[] args) {
+
+        VegItem f1 = new VegItem("DalMakhni", "veg", 100, 10);
+        NonVegItem f2 = new NonVegItem("Butter-Paneer", "nonVeg", 1000, 10);
+        VegItem f3 = new VegItem("Pizza", "veg", 400, 1);
+        VegItem f4 = new VegItem("Pizza", "veg", 400, 3);
+
+        List<FoodItem> items = new ArrayList<>();
+        items.add(f1);
+        items.add(f2);
+        items.add(f3);
+        items.add(f4);
+
+        Order order = new Order(items);
+        order.placeOrder(0, 0, 0);
+    }
 }
+
