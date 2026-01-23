a5703044da33a5b93e26b74a268ae4e122b63805
Xxdhruvshriwastav <rudrashriwastav99393@gmail.com>
2026-01-23 11:39:28 +0530

    [Ashish] Added dsa practice and java oops practice of FitnessTracker

--- dsa-practice/seneriobased questions/Senerioabasedproblems/src/FitnessTracker/FitnessTracker.java
+++ dsa-practice/seneriobased questions/Senerioabasedproblems/src/FitnessTracker/FitnessTracker.java
@@ -9,12 +9,14 @@ public class FitnessTracker {
 	public static void bubbleSort(HashMap<String, Integer> map) {
 
 		// convert hashmap to list for index because hashpmap has no index but list has
+		
 		List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
 
 		int n = list.size();
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = 0; j < n - i - 1; j++) {
 				if (list.get(j).getValue() > list.get(j + 1).getValue()) {
+					
 					// swap
 					Map.Entry<String, Integer> temp = list.get(j);
 					list.set(j, list.get(j + 1));
@@ -34,6 +36,7 @@ public class FitnessTracker {
 		Scanner sc = new Scanner(System.in);
 
 		// using hashmap to store data via user input and bubble sort for sorting
+		
 		HashMap<String, Integer> fitnessData = new HashMap<>();
 
 		for (int i = 0; i < 5; i++) {
